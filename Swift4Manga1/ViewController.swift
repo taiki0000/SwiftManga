292f7398bcfa8f037c092fd23f00203b7ff49b44
taiki <nikora0425@icloud.com>
2018-10-27 10:19:37 +0900

    Initial Commit

---
+++ Swift4Manga1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift4Manga1
+//
+//  Created by taiki on 2018/10/27.
+//  Copyright © 2018年 taiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
